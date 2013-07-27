e7072321bbd54336d99d3e76a1b90a6d84982a90
dqj <dqj@dqj-Inspiron-N4050.(none)>
2013-07-27 23:05:41 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '17fea9429ba6169fa86cd3e012daeb6d8554687b3e6738436340ef2d374cb4cc0365d8157aafd742d00bbc460757ba8ea86f3b0dd4d16ef38fa661193df5ac4c'
